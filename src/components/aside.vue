1276cc7a92868627afce19c25342d0898289a115
Sobirov S <sobiroff23@gmail.com>
2024-12-10 11:43:32 +0500

    changed inside adises

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -1,8 +1,13 @@
 <template>
   <aside class="aside-block">
-    <div>
-      <h3>Todo: рабочее пространство Бесплатно</h3>
-      <button><img src="../../public/icons/arrow_left.svg" alt=""></button>
+    <div class="box">
+      <div class="aside-box">
+        <h3>
+          TODO: рабочее пространство
+          <p>Бесплатно</p>
+        </h3>
+        <button><img  src="../../public/icons/arrow_left.svg" alt="arrow" /></button>
+      </div>
     </div>
   </aside>
 </template>
@@ -13,10 +18,46 @@ export default {
 };
 </script>
 
+
+
+
 <style lang="scss">
 .aside-block {
-  width: 300px;
+  width: 500px;
   height: 100%;
   background-color: #161a1d;
+
+  .box {
+    padding-left: 15px;
+
+    .aside-box {
+      display: flex;
+      align-items: center;
+        justify-content: space-between;
+        padding: 0px 15px;
+      h3 {
+        font-size: 18px;
+        color: #9fadbc;
+
+        p {
+          font-size: 14px;
+          font-weight: 400;
+        }
+      }
+
+      button {
+        border: none;
+        padding: 10px 10px;
+        height: 32px;
+        width: 32px;
+        background-color: #161a1d;
+
+        img {
+            height: 22px;
+            width: 22px;
+        }
+      }
+    }
+  }
 }
 </style>
