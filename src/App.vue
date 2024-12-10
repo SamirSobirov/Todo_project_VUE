39e5570b8b894fb127d63654148197e4ecc8b7b5
Sobirov S <sobiroff23@gmail.com>
2024-12-10 10:51:06 +0500

    added scss

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>
+    
+<headerVue />
+
+  </div>
+</template>
+
+<style scoped></style>
+
+
+
+
+<script>
+import headerVue from './components/header.vue';
+
+
+
+export default {
+  name: "App",
+  components: {
+    headerVue
+
+  },
+};
+</script>
---
+++ src/components/header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header class="header">
+    <nav>
+      <h1>Trello</h1>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderVue",
+};
+</script>
+
+
+
+<style lang="scss">
+.header {
+    height: 100px;
+    width: 100%;
+    background-color: #1D2125;
+
+}
+</style>
