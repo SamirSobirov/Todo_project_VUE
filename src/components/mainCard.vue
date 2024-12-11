d9e5ce3cf0b232e27968a6df3fe612a728c41caa
Sobirov S <sobiroff23@gmail.com>
2024-12-11 15:30:23 +0500

    changed body color

--- src/components/mainCard.vue
+++ src/components/mainCard.vue
@@ -29,7 +29,7 @@
 .Cards div {
   height: 300px;
   width: 350px;
-  background-color: #000000;
+  background-color: #000000ee;
   padding: 20px;
   border-radius: 8px;
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
