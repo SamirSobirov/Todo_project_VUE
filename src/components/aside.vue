b4d48d7a06cfda6410307e61cb5b047faabb3a49
Sobirov S <sobiroff23@gmail.com>
2024-12-11 11:13:52 +0500

    changed stylies

--- src/components/aside.vue
+++ src/components/aside.vue
@@ -1,53 +1,44 @@
 <template>
-  <aside v-if="isAsideVisible" class="aside-block">
-    <div class="box">
-      <div class="aside-box1">
-        <h3>
-          TODO: рабочее пространство
-          <p>Бесплатно</p>
-        </h3>
-        <button @click="toggleAside">
-          <img src="../../public/icons/arrow_left.svg" alt="arrow" />
-        </button>
+  <div>
+    <aside :class="{'aside-block': true, 'hidden': !isAsideVisible}">
+      <div class="box">
+        <div class="aside-box1">
+          <h3>
+            TODO: рабочее пространство
+            <p>Бесплатно</p>
+          </h3>
+          <button @click="toggleAside">
+            <img src="../../public/icons/arrow_left.svg" alt="arrow" />
+          </button>
+        </div>
+        <hr />
+        <div class="aside-box3">
+          <button class="aside-button_1">
+            <img src="../../public/icons/board.svg" alt="" />
+            <p>Доски</p>
+          </button>
+          <button class="aside-button_2">
+            <img src="../../public/icons/participants.svg" alt="" />
+            <p>Участники</p>
+          </button>
+        </div>
+        <div class="aside-box2">
+          <p>Режимы просмотра рабочего пространства...</p>
+        </div>
+        <div class="aside-box3">
+          <button class="aside-button_2">
+            <img src="../../public/icons/participants.svg" alt="">
+            <p>Таблица</p>
+          </button>
+          <button class="aside-button_2" style="padding-left: 15px;">
+            <img src="../../public/icons/participants.svg" alt="">
+            <p>Календарь</p>
+          </button>
+        </div>
       </div>
-
-      <hr />
-
-
-      <div class="aside-box3">
-      <button class="aside-button_1">
-        <img src="../../public/icons/board.svg" alt="" />
-        <p>Доски</p>
-      </button>
-
-      <button class="aside-button_2">
-        <img src="../../public/icons/participants.svg" alt="" />
-        <p>Участники</p>
-      </button>
-
-    </div>
-
-      <div class="aside-box2">
-        <p>Режимы просмотра рабочего пространства...</p>
-      </div>
-
-
-      <div class="aside-box3">
-        <button class="aside-button_2">
-          <img src="../../public/icons/participants.svg" alt="">
-          <p>Таблица</p>
-
-        </button>
-
-        <button class="aside-button_2" style="padding-left: 15px;">
-          <img src="../../public/icons/participants.svg" alt="">
-          <p>Календарь</p>
-        </button>
-      </div>
-    </div>
-  </aside>
-
-  <button v-else @click="toggleAside" class="open-aside">Open Aside</button>
+    </aside>
+    <button v-if="!isAsideVisible" @click="toggleAside" class="open-aside">Open Aside</button>
+  </div>
 </template>
 
 <script lang="ts">
@@ -70,6 +61,7 @@ export default defineComponent({
 });
 </script>
 
+
 <style lang="scss">
 .aside-block {
   width: 500px;
@@ -77,6 +69,15 @@ export default defineComponent({
   background-color: #161a1d;
   display: flex;
   flex-direction: column;
+  position: fixed;
+  top: 0;
+  left: 0;
+  transform: translateX(0); 
+  transition: transform 0.3s ease-in-out;
+
+  &.hidden {
+    transform: translateX(-100%);
+  }
 
   .box {
     padding: 0 10px;
@@ -160,8 +161,8 @@ export default defineComponent({
       }
 
       &:hover {
-          background-color: #424242;
-        }
+        background-color: #424242;
+      }
     }
 
     .aside-box2 {
@@ -170,14 +171,28 @@ export default defineComponent({
       align-items: center;
       padding: 10px;
 
-   
-
       p {
         color: white;
         font-size: 20px;
       }
     }
-    
+  }
+}
+
+.open-aside {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 4px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:hover {
+    background-color: #0056b3;
   }
 }
 
