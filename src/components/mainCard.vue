274463d8f0b9a2f9b48a40f4fe83af41c3e7e09f
Sobirov S <sobiroff23@gmail.com>
2024-12-11 16:23:16 +0500

    fixed

--- src/components/mainCard.vue
+++ src/components/mainCard.vue
@@ -1,16 +1,22 @@
 <template>
-  <main class="Cards">
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+  <main class="Cards_box">
+    <div class="card">
+        <h3>Нужно сделать</h3>
+        <div class="inside"></div>
+
+
+        <button>  
+            <p>+</p>
+            <p class="secondP">Добавить карточку</p>
+        </button>
+        
+    </div>
+   
   </main>
 </template>
 
 <style lang="scss">
-.Cards {
+.Cards_box {
   margin-top: 30px;
   padding-left: 500px;
   display: grid;
@@ -26,14 +32,40 @@
   }
 }
 
-.Cards div {
-  height: 300px;
-  width: 350px;
+.Cards_box .card {
+    align-items: center;
+    justify-content: center;
+  height: 100%;
+  width: 280px;
   background-color: #000000ee;
-  padding: 20px;
+  padding: 20px 20px;
   border-radius: 8px;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  box-shadow: 9px 12px 15px rgba(0, 0, 0, 0.8);
+  h3 {
+color: white;
+  }
+  p {
+    color: white;
+  }
 }
+
+button {
+    margin-top: 15px;
+    display: flex;
+    width: 250px;
+    gap: 12px;
+    border: none;
+    background-color: #00000000;
+    font-size: 14px;
+    padding: 10px 10px;
+    border-radius: 12px;
+
+    &:hover {
+          background-color: #424242;
+        }
+}
+
+
 </style>
 
 <script lang="ts">
