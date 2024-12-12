353ca4c8da4a34c83b16a4b3a84d78ac6beed476
Sobirov S <sobiroff23@gmail.com>
2024-12-12 15:32:45 +0500

    change scss

--- src/components/header.vue
+++ src/components/header.vue
@@ -18,48 +18,4 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
-.header {
-  height: 70px;
-  width: 100%;
-  background-color: #1d2125;
-
-  .nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 100%;
-    padding: 0 20px;
-
-    h1 {
-      color: #fff;
-    }
-
-    .box {
-      display: flex;
-      gap: 10px;
-      button {
-        background-color: #007bff;
-        color: #fff; /* Цвет текста */
-        border: none;
-        padding: 10px 20px;
-        font-size: 16px;
-        cursor: pointer;
-        transition: background-color 0.3s ease;
-      }
-
-      button:hover {
-        background-color: #73b7ff;
-      }
-
-      input {
-        padding: 5px 20px;
-        border-radius: 6px;
-        border: none;
-        background-color: #22272b;
-        color: white;
-      }
-    }
-  }
-}
-</style>
+<style lang="scss"></style>
--- src/components/main.vue
+++ src/components/main.vue
@@ -20,31 +20,4 @@ export default defineComponent({
 });
 </script>
 
-<style lang="scss">
-.main_header {
-  display: flex;
-  align-items: center;
-  height: 56px;
-  width: 100%;
-  background-color: #00000058;
-  transition: margin-left 0.3s ease;
-
-  &.shifted {
-    transform: translateX(-250px);
-  }
-
-  h1 {
-    padding-left: 50px;
-    font-size: 30px;
-    color: white;
-  }
-
-  &.with-aside {
-    padding-left: 450px; 
-  }
-
-  &.without-aside {
-    margin-left: 0;
-  }
-}
-</style>
+<style lang="scss"></style>
