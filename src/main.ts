ed6ec21db4a2ca32e032a9719de180481c5ada6c
Sobirov S <sobiroff23@gmail.com>
2024-12-14 15:37:22 +0500

    changed name of files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-import './assets/main.css'
-import './assets/style.scss'
+import './assets/css/main.css'
+import './assets/scss/style.scss'
 
 import { createApp } from 'vue'
 import App from './App.vue'
